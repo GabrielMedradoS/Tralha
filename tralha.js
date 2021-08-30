e5078982cead5448c5fc0e438ef168d36ce24c41
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-08-29 23:17:31 -0300

    CheckOrder

--- tralha.js
+++ tralha.js
@@ -1,22 +1,23 @@
-let player = null;
+let player, winner = null;
 
 function pickBox(id) {
     let box = document.getElementById(id); 
-
-    box.innerHTML = player;
-    box.style.color = '#000';
 
     if (box.innerHTML !== '-') { /* condiçao para nao jogar encima do valor */
         return;
     }
 
+    box.innerHTML = player;
+    box.style.color = '#000';
+
     if(player === 'X') {
         player = 'O';
-    } else {
+    }else if (player === 'O'){
         player = 'X';
     }
 
     switchPlayer(player);
+    checkWinner();
 }
 
 function switchPlayer(valor) {
@@ -40,10 +41,50 @@ function checkWinner() {
     let box7 = document.getElementById(7);
     let box8 = document.getElementById(8);
     let box9 = document.getElementById(9);
+
+    if ( checkOrder(box1, box2, box3)) {
+        colorChange(box1, box2, box3);
+        changeWinner(box1)
+        return
+    }
+
+    if ( checkOrder(box4, box5, box6)) {
+        colorChange(box4, box5, box6);
+        changeWinner(box4)
+        return
+    }
+
+    if ( checkOrder(box7, box8, box9)) {
+        colorChange(box7, box8, box9);
+        changeWinner(box7)
+        return
+    }
+
+    if ( checkOrder(box1, box4, box7)) {
+        colorChange(box1, box4, box7);
+        changeWinner(box1)
+        return
+    }
 }
 
-function checkOrder(box1, box2, box3) {
+function checkOrder(checkBox1, checkBox2, checkBox3) {
     let order = false;
 
+    if (checkBox1.innerHTML !== '-' && checkBox1.innerHTML === checkBox2.innerHTML && checkBox2.innerHTML === checkBox3.innerHTML) {
+        order = true;
+    }
 
+    return order;
+}
+
+function colorChange(box1, box2, box3) {
+    box1.style.background = '#0f0';
+    box2.style.background = '#0f0';
+    box3.style.background = '#0f0';
+}
+
+function changeWinner(box) {
+    let selectedWinner = document.getElementById('selectedWinner');
+    winner = box.innerHTML;
+    selectedWinner.innerHTML = winner;
 }
